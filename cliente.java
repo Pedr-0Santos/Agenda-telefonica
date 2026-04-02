ada6e528ffe13cf8b577a1aa5bcb4c7689e66fb4
Pedr-0Santos <pedro.rocha.delima2007@gmail.com>
2026-04-02 11:41:05 -0300

    Add files via upload

---
+++ cliente.java
@@ -0,0 +1,54 @@
+
+public class cliente {
+   
+	private String nome;
+	private String cpf;
+	private String email;
+	private String cep;
+	private String telefone;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+	
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getCep() {
+		return cep;
+	}
+	
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	
+	public String getTelefone() {
+		return telefone;
+	}
+	
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	
+	
+	
+}
